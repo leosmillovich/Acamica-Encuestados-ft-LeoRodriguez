ca43fec6242b6d957f4abe65317fdaa0518ded8f
unknown <enriqueleonardorodriguez@gmail.com>
2020-01-29 21:22:30 -0300

    segundo paso listo la puta madre

--- js/modelo/modelo.js
+++ js/modelo/modelo.js
@@ -1,30 +1,47 @@
 /*
  * Modelo
  */
-var Modelo = function() {
+var Modelo = function () {
   this.preguntas = [];
   this.ultimoId = 0;
 
   //inicializacion de eventos
   this.preguntaAgregada = new Evento(this);
+  this.preguntaEliminada = new Evento(this);
 };
 
 Modelo.prototype = {
   //se obtiene el id más grande asignado a una pregunta
-  obtenerUltimoId: function() {
+  obtenerUltimoId: function () {
+    var id;
+    var ultimoValorId = this.preguntas.length;
+    ultimoValorId > 0 ? id = ultimoValorId : id = 0;
+    return id;
   },
 
   //se agrega una pregunta dado un nombre y sus respuestas
-  agregarPregunta: function(nombre, respuestas) {
+  agregarPregunta: function (nombre, respuestas) {
     var id = this.obtenerUltimoId();
     id++;
-    var nuevaPregunta = {'textoPregunta': nombre, 'id': id, 'cantidadPorRespuesta': respuestas};
+    var nuevaPregunta = {
+      'textoPregunta': nombre,
+      'id': id,
+      'cantidadPorRespuesta': respuestas
+    };
     this.preguntas.push(nuevaPregunta);
     this.guardar();
     this.preguntaAgregada.notificar();
   },
 
+  borrarPregunta: function (id) {
+    let preguntaBuscada = this.preguntas.find((pregunta) => pregunta.id == id);
+    let indexId = this.preguntas.indexOf(preguntaBuscada);
+    this.preguntas.splice(indexId, 1);
+    this.preguntaEliminada.notificar();
+    return this.preguntas;
+  },
+
   //se guardan las preguntas
-  guardar: function(){
+  guardar: function () {
   },
 };
--- js/vistas/vistaAdministrador.js
+++ js/vistas/vistaAdministrador.js
@@ -1,22 +1,27 @@
 /*
- * Vista administrador
- */
-var VistaAdministrador = function(modelo, controlador, elementos) {
+* Vista administrador
+*/
+var VistaAdministrador = function (modelo, controlador, elementos) {
   this.modelo = modelo;
   this.controlador = controlador;
   this.elementos = elementos;
   var contexto = this;
 
   // suscripción de observadores
-  this.modelo.preguntaAgregada.suscribir(function() {
+  this.modelo.preguntaAgregada.suscribir(function () {
     contexto.reconstruirLista();
   });
+
+  this.modelo.preguntaEliminada.suscribir(function () {
+    contexto.reconstruirLista();
+  });
+
 };
 
 
 VistaAdministrador.prototype = {
   //lista
-  inicializar: function() {
+  inicializar: function () {
     //llamar a los metodos para reconstruir la lista, configurar botones y validar formularios
     validacionDeFormulario();
     //paso 1
@@ -24,54 +29,63 @@ VistaAdministrador.prototype = {
     this.configuracionDeBotones();
   },
 
-  construirElementoPregunta: function(pregunta){
+  construirElementoPregunta: function (pregunta) {
     var contexto = this;
     var nuevoItem;
-    //esto se agrego en clases- paso 1
-    var nuevoItem =  $('<li></li>');
-    nuevoItem.addClass('list-group-item');
-    nuevoItem.attr('id', pregunta.id)
-    nuevoItem.text(pregunta.textoPregunta)
     //completar
     //asignar a nuevoitem un elemento li con clase "list-group-item", id "pregunta.id" y texto "pregunta.textoPregunta"
+    //esto se agrego en clases- paso 1
+    var nuevoItem = $('<li></li>');
+    nuevoItem.addClass('list-group-item');
+    nuevoItem.attr('id', pregunta.id)
+    nuevoItem.text('pregunta.textoPregunta');
     var interiorItem = $('.d-flex');
     var titulo = interiorItem.find('h5');
     titulo.text(pregunta.textoPregunta);
-    interiorItem.find('small').text(pregunta.cantidadPorRespuesta.map(function(resp){
+    interiorItem.find('small').text(pregunta.cantidadPorRespuesta.map(function (resp) {
       return " " + resp.textoRespuesta;
     }));
     nuevoItem.html($('.d-flex').html());
     return nuevoItem;
   },
 
-  reconstruirLista: function() {
+  reconstruirLista: function () {
     var lista = this.elementos.lista;
     lista.html('');
     var preguntas = this.modelo.preguntas;
-    for (var i=0;i<preguntas.length;++i){
+    for (var i = 0; i < preguntas.length; ++i) {
       lista.append(this.construirElementoPregunta(preguntas[i]));
     }
   },
 
-  configuracionDeBotones: function(){
+  configuracionDeBotones: function () {
     var e = this.elementos;
     var contexto = this;
-
     //asociacion de eventos a boton
-    e.botonAgregarPregunta.click(function() {
+    e.botonAgregarPregunta.click(function () {
       var value = e.pregunta.val();
       var respuestas = [];
 
-      $('[name="option[]"]').each(function() {
-        //completar
+      $('[name="option[]"]').each(function () {
+        var respuesta = $(this).val();
+        var cantVotos = 0;
+        var nuevaRespuesta = { 'textoRespuesta': respuesta, 'cantidad': cantVotos };
+        respuestas.push(nuevaRespuesta);
       })
       contexto.limpiarFormulario();
       contexto.controlador.agregarPregunta(value, respuestas);
     });
+    //agrego el evento para el boton borrar pregunta 
+    e.botonBorrarPregunta.click(function () {
+      var id = parseInt($('.list-group-item.active').attr('id'));
+      console.log(id)
+      contexto.controlador.borrarPregunta(id);
+    });
+
     //asociar el resto de los botones a eventos
   },
 
-  limpiarFormulario: function(){
+  limpiarFormulario: function () {
     $('.form-group.answer.has-feedback.has-success').remove();
   },
 };
